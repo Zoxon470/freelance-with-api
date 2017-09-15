2bc4d5581cc100f8b0cc3b01faac36cef262dddc
Zoxon470 <zoxon470@gmail.com>
2017-09-15 13:43:19 +0600

    Added assignee info

--- freelance/api/serializers.py
+++ freelance/api/serializers.py
@@ -3,19 +3,32 @@ from users.models import User
 from task.models import Task
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'user_type', 'balance')
+
+
 class TaskSerializer(serializers.ModelSerializer):
+    assignee = serializers.SerializerMethodField()
 
     class Meta:
         model = Task
         fields = ('id', 'title', 'description', 'money', 'assignee', 'created_by')
 
+    def get_assignee(self, obj):
+        return {
+            'id': obj.assignee.id,
+            'username': obj.assignee.username,
+            'first_name': obj.assignee.first_name,
+            'last_name': obj.assignee.last_name,
+            'user_type': obj.assignee.user_type,
+            'balance': obj.assignee.balance
+        }
 
-class UserSerializer(serializers.ModelSerializer):
-    tasks = TaskSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'user_type', 'balance', 'tasks')
+
 
 
 
